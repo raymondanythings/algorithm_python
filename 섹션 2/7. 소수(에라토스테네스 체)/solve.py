9ab07f7921350abc77e0935f2ddad8eede60e14d
Raymond <raymondanytrhing@gmail.com>
2022-03-20 23:26:41 +0900

    소수(에라토스테네스 체)

--- 섹션 2/7. 소수(에라토스테네스 체)/solve.py
+++ 섹션 2/7. 소수(에라토스테네스 체)/solve.py
@@ -5,9 +5,9 @@ import sys
 for index in range(1, 6):
     with open(f"in{index}.txt", "rt", encoding="utf8") as sys.stdin:
         n = int(input())
-        arr = [1, 1] + [0] * (n - 1)
+        arr = [0] * (n + 1)
         primes = []
-        for i in range(0, n + 1):
+        for i in range(2, n + 1):
             if arr[i] == 0:
                 primes.append(i)
                 for j in range(i, n + 1, i):
