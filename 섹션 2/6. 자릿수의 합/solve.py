86ab18649d433fcd1495adad25d4ff220c7f4ff4
Raymond <raymondanytrhing@gmail.com>
2022-03-20 22:03:11 +0900

    자릿수의합 2가지

--- 섹션 2/6. 자릿수의 합/solve.py
+++ 섹션 2/6. 자릿수의 합/solve.py
@@ -3,17 +3,31 @@ import sys
 
 def digit_sum(number):
     result = 0
-    for i in number:
+    for i in str(number):
         result += int(i)
     return result
+
+
+def digit_sum2(number):
+    sum = 0
+    while number > 0:
+        sum += number % 10
+        number //= 10
+    return sum
 
 
 for index in range(1, 6):
     with open(f"in{index}.txt", "rt", encoding="utf8") as sys.stdin:
         n = input()
-        k = list(input().split())
+        k = list(map(int, input().split()))
         li = []
+        mx = -1
         for x in k:
-            li.append(digit_sum(x))
+            data = digit_sum(x)
+            li.append(data)
+            if data > mx:
+                mx = data
+                res = x
         idx = li.index(max(li))
         print(k[idx])
+        print(res)
