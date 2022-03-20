45296a8b769a27f13d926632eab96fd2b209ed0e
Raymond <raymondanytrhing@gmail.com>
2022-03-20 23:25:01 +0900

    소수(에라토스테네스 체)

---
+++ 섹션 2/7. 소수(에라토스테네스 체)/solve.py
@@ -0,0 +1,15 @@
+import sys
+
+
+# 에라토스테네스 체
+for index in range(1, 6):
+    with open(f"in{index}.txt", "rt", encoding="utf8") as sys.stdin:
+        n = int(input())
+        arr = [1, 1] + [0] * (n - 1)
+        primes = []
+        for i in range(0, n + 1):
+            if arr[i] == 0:
+                primes.append(i)
+                for j in range(i, n + 1, i):
+                    arr[j] = 1
+        print(len(primes))
