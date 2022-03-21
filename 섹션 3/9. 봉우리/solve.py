d451963bca0530d2cacd867f55133160778c8a54
raymondanythings <akdfhr2@gmail.com>
2022-03-21 18:31:26 +0900

    judge deco. with args

---
+++ 섹션 3/9. 봉우리/solve.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.path)
+sys.path.insert(0, "/Users/yonghyunyeob/Documents/algorithm/inflearn/pythonalgorithm_formac")
+from judge import judge
+
+
+@judge
+def solve():
+    return
+
+
+solve()
