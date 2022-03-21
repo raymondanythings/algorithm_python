6ef83b58c73110bca70109ee7f68160533d1c595
raymondanythings <akdfhr2@gmail.com>
2022-03-21 19:18:53 +0900

    봉우리

--- boiler.py
+++ boiler.py
@@ -1,11 +1,11 @@
 import sys
 
-print(sys.path)
+
 sys.path.insert(0, "/Users/yonghyunyeob/Documents/algorithm/inflearn/pythonalgorithm_formac")
 from judge import judge
 
 
-@judge
+@judge()
 def solve():
     return
 
--- 섹션 3/9. 봉우리/solve.py
+++ 섹션 3/9. 봉우리/solve.py
@@ -5,9 +5,23 @@ sys.path.insert(0, "/Users/yonghyunyeob/Documents/algorithm/inflearn/pythonalgor
 from judge import judge
 
 
-@judge
+@judge()
 def solve():
-    return
+    n = int(input())
+    li = [list(map(int, input().split())) for _ in range(n)]
+    li.append([0] * n)
+    li.insert(0, [0] * n)
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    for x in li:
+        x.append(0)
+        x.insert(0, 0)
+    result = 0
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if all(li[i][j] > li[i + dx[k]][j + dy[k]] for k in range(4)):
+                result += 1
+    return result
 
 
 solve()
