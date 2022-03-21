afcf58282ebe5a83185ea25243cb50ca2b3628da
Raymond <raymondanytrhing@gmail.com>
2022-03-21 18:28:39 +0900

    곳감

--- judge.py
+++ judge.py
@@ -2,17 +2,21 @@ import sys
 import os
 
 
-def judge(func):
-    def wrapper():
-        for index in range(1, 6):
-            with open(f"{os.getcwd()}/in{index}.txt", "rt", encoding="utf8") as sys.stdin:
-                result = str(func())
-            with open(f"{os.getcwd()}/out{index}.txt", "rt", encoding="utf8") as re:
-                current = re.read()
-                txt = f"Case #{index} : "
-                if result == current:
-                    print(txt, "Clear!")
-                else:
-                    print(txt, "Error!")
+def judge(start=1, end=6):
+    def set_deco(func):
+        def wrapper():
+            for index in range(start, end):
+                with open(f"{os.getcwd()}/in{index}.txt", "rt", encoding="utf8") as sys.stdin:
+                    result = str(func())
+                with open(f"{os.getcwd()}/out{index}.txt", "rt", encoding="utf8") as re:
+                    current = re.read()
+                    txt = f"Case #{index} : "
+                    if result == current:
+                        print(txt, "Clear!")
+                    else:
+                        print(txt, "Error!")
+            return
 
-    return wrapper
+        return wrapper
+
+    return set_deco
