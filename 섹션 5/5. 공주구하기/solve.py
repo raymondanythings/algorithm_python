2f4439f668457a39dddd6a85807cb72988886cd0
raymondanythings <akdfhr2@gmail.com>
2022-03-25 23:57:17 +0900

    공주 구하기

--- 섹션 5/5. 공주구하기/solve.py
+++ 섹션 5/5. 공주구하기/solve.py
@@ -1,5 +1,5 @@
 import sys
-
+from collections import deque
 
 sys.path.insert(0, "/Users/yonghyunyeob/Documents/algorithm/inflearn/pythonalgorithm_formac")
 from judge import judge
@@ -7,7 +7,32 @@ from judge import judge
 
 @judge()
 def solve():
-    return
+    n, k = map(int, input().split())
+    li = [i for i in range(1, n + 1)]
+
+    # 방법 1
+
+    # Q = deque(li)
+    # cnt = 0
+    # while len(Q) > 1:
+    #     cnt += 1
+    #     tmp = Q.popleft()
+    #     if cnt == k:
+    #         cnt = 0
+    #     else:
+    #         Q.append(tmp)
+    # return Q[0]
+
+    # 방법 2
+
+    dq = deque(li)
+    while dq:
+        for _ in range(k - 1):
+            cur = dq.popleft()
+            dq.append(cur)
+        dq.popleft()
+        if len(dq) == 1:
+            return dq[0]
 
 
 solve()
