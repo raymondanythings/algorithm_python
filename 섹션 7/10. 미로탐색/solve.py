d33928776c3b244b7a83a2d37daf414ef34efe03
raymondanythings <akdfhr2@gmail.com>
2022-04-08 21:46:41 +0900

    미로탐색(DFS)

--- 섹션 7/10. 미로탐색/solve.py
+++ 섹션 7/10. 미로탐색/solve.py
@@ -4,10 +4,32 @@ import os
 sys.path.insert(0, "/".join(os.getcwd().split("/")[:-2]))
 from judge import judge
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+def DFS(x, y):
+    global cnt, board
+    if x == 6 and y == 6:
+        cnt += 1
+    else:
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            if 0 <= xx <= 6 and 0 <= yy <= 6 and not board[xx][yy]:
+                board[xx][yy] = 1
+                DFS(xx, yy)
+                board[xx][yy] = 0
+
 
 @judge()
 def solve():
-    return
+    global cnt, board
+    board = [list(map(int, input().split())) for _ in range(7)]
+    cnt = 0
+    board[0][0] = 1
+    DFS(0, 0)
+    return cnt
 
 
 solve()
