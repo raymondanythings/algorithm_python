d1c45d361aece8ea5724bb748c5cd106f682f430
raymondanythings <akdfhr2@gmail.com>
2022-04-09 13:49:45 +0900

    네트워크선 자르기(Top down)

--- 섹션 8/1, 2. 네트워크 선 자르기/solve.py
+++ 섹션 8/1, 2. 네트워크 선 자르기/solve.py
@@ -5,31 +5,35 @@ sys.path.insert(0, "/".join(os.getcwd().split("/")[:-2]))
 from judge import judge
 
 
-# def DFS(L, cnt):
-#     global n, re
-#     if cnt > n:
-#         return
-#     elif cnt == n:
-#         re += 1
-#     else:
-#         DFS(L + 1, cnt + 1)
-#         DFS(L + 1, cnt + 2)
+def DFS(L):
+    global n, dy
+    if dy[L]:
+        return dy[L]
+    if L == 1 or L == 2:
+        return L
+    else:
+        dy[L] = DFS(L - 1) + DFS(L - 2)
+        return dy[L]
 
 
 @judge()
 def solve():
-    global n, re
+    global n, dy
     n = int(input())
     re = 0
 
     dy = [0] * (n + 1)
-    dy[1] = 1
-    dy[2] = 2
-    for i in range(3, n + 1):
-        dy[i] = dy[i - 1] + dy[i - 2]
-    re = dy[-1]
-    # DFS(0, 0)
-    # print(re)
+
+    ## Bottom up
+
+    # dy[1] = 1
+    # dy[2] = 2
+    # for i in range(3, n + 1):
+    #     dy[i] = dy[i - 1] + dy[i - 2]
+    # re = dy[-1]
+
+    # Top down (메모리제이션)
+    re = DFS(n)
     return re
 
 
