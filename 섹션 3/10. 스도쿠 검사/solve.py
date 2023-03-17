826f120ec5eed9c2a7e6da907fb23dba4358b979
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-03-17 14:08:51 +0900

    [2회차] 섹션3- 격자판 회문수

--- 섹션 3/10. 스도쿠 검사/solve.py
+++ 섹션 3/10. 스도쿠 검사/solve.py
@@ -1,58 +1,41 @@
 import sys
+import os
 
 
-sys.path.insert(0, "/Users/yonghyunyeob/Documents/algorithm/inflearn/pythonalgorithm_formac")
+if os.name == 'nt':
+    sys.path.insert(0, "\\".join(os.getcwd().split("\\")[:-2]))
+else:
+    sys.path.insert(0, "/".join(os.getcwd().split("/")[:-2]))
 from judge import judge
-
-
-def check(a):
-    for i in range(9):
-        ch1 = [0] * 10
-        ch2 = [0] * 10
-        for j in range(9):
-            ch1[a[i][j]] = 1
-            ch2[a[j][i]] = 1
-        if sum(ch1) != 9 or sum(ch2) != 9:
-            return False
-    for i in range(3):
-        for j in range(3):
-            ch3 = [0] * 10
-            for k in range(3):
-                for s in range(3):
-                    ch3[a[i * 3 + k][j * 3 + s]] = 1
-            if sum(ch3) != 9:
-                return False
-    return True
 
 
 @judge()
 def solve():
-    li = [list(map(int, input().split())) for _ in range(9)]
+    board = [list(map(int, input().split())) for _ in range(9)]
 
-    # 방법 1
+    for i in range(9):
+        ch1 = [0] * 10
+        ch2 = [0] * 10
+        for j in range(9):
+            if ch1[board[i][j]] == 0:
+                ch1[board[i][j]] = 1
+            else:
+                return 'NO'
+            if ch2[board[j][i]] == 0:
+                ch2[board[j][i]] = 1
+            else:
+                return 'NO'
+    for i in range(3):
+        for j in range(3):
+            ch = [0] * 10
+            for x in range(3):
+                for y in range(3):
+                    if ch[board[x + i * 3][y + j * 3]] == 0:
+                        ch[board[x + i * 3][y + j * 3]] = 1
+                    else:
+                        return 'NO'
 
-    # col = [[] for _ in range(9)]
-    # box = [[] for _ in range(9)]
-    # for page, x in enumerate(li):
-    #     if len(set(x)) != 9:
-    #         return "NO"
-    #     for i in range(3):
-    #         box[((page // 3) * 3) + i] += x[i * 3 : i * 3 + 3]
-    #     for j in range(9):
-    #         col[j].append(x[j])
-    # for x in col:
-    #     if len(set(x)) != 9:
-    #         return "NO"
-    # for x in box:
-    #     if len(set(x)) != 9:
-    #         return "NO"
-    # return "YES"
-
-    # 방법 2
-
-    if check(li):
-        return "YES"
-    return "NO"
+    return 'YES'
 
 
 solve()
--- 섹션 3/11. 격자판 회문수/solve.py
+++ 섹션 3/11. 격자판 회문수/solve.py
@@ -1,36 +1,46 @@
 import sys
+import os
 
 
-sys.path.insert(0, "/Users/yonghyunyeob/Documents/algorithm/inflearn/pythonalgorithm_formac")
+if os.name == 'nt':
+    sys.path.insert(0, "\\".join(os.getcwd().split("\\")[:-2]))
+else:
+    sys.path.insert(0, "/".join(os.getcwd().split("/")[:-2]))
 from judge import judge
 
 
 @judge()
 def solve():
+    answer = 0
     board = [list(map(int, input().split())) for _ in range(7)]
-    cnt = 0
+    for i in range(7):
+        for j in range(3):
+            row = board[i][j:j+5]
+            if row[0] == row[4] and row[1] == row[3]:
+                answer += 1
+    for i in range(7):
+        for j in range(3):
+            col = [board[j+x][i] for x in range(5)]
+            if col[0] == col[4] and col[1] == col[3]:
+                answer += 1
 
-    # 방법 1
-    # for i in range(7):
-    #     cols = [board[k][i] for k in range(7)]
-    #     for j in range(3):
-    #         row = board[i][j : j + 5]
-    #         col = cols[j : j + 5]
-    #         if row == row[::-1] or col == col[::-1]:
-    #             cnt += 1
+    return answer
+# @judge()
+# def solve():
+#     answer = 0
+#     board = [list(map(int, input().split())) for _ in range(7)]
+#     for i in range(3):
+#         for j in range(7):
+#             tmp = board[j][i:i+5]
+#             if tmp == tmp[::-1]:
+#                 answer += 1
+#             for k in range(2):
+#                 if board[i+k][j] != board[i+5-k-1][j]:
+#                     break
+#             else:
+#                 answer += 1
 
-    # 방법 2
-    for i in range(3):
-        for j in range(7):
-            tmp = board[j][i : i + 5]
-            if tmp == tmp[::-1]:
-                cnt += 1
-            for k in range(2):
-                if board[i + k][j] != board[i + 5 - k - 1][j]:
-                    break
-            else:
-                cnt += 1
-    return cnt
+#     return answer
 
 
 solve()
